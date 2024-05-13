1140359250c0fba6159180b351af225a5418a7a4
ltrix07 <skotarenko138@gmail.com>
2024-05-13 17:10:27 +0300

    report about upload to AMZ fix

--- main_settings.py
+++ main_settings.py
@@ -7,9 +7,9 @@ transient_vice_for_threads = 50000
 
 """ --- Amazon settings --- """
 amz_creds = {
-    "lwa_app_id": 'amzn1.application-oa2-client.a93ba3c1fc6c473093b4045304e34fba',
-    "lwa_client_secret": 'amzn1.oa2-cs.v1.9191c3fae6cda6c615f40a0bb318dcb1cd391b16d3a21b84c7b08cd1caef3216',  # Amazon Creds
-    "refresh_token": 'Atzr|IwEBIENECGtwyidHYfCw7zwZbdwIWODfIe71ZQ-bwR5qy69ijsLnpW58SbykXmzJO4KuwANAqPZGXss6dPhSErZUTQ_-W2ue_R-ms-qvX7XMMN8iKYGrrCB5Oy9A8cLsTzHS8O6VwW13_kKNd3Dkl71I3XZfdpsp_i3YmcxpExED5d3Svz3Ptw3gTzV_iZi1rOL9dRSdqqm4iPTLcTr_5MVFi8cKQB75S5RHfOtICoxswdtn3OVGZsgU9HYusfUySlDHqZuCHlFiy4YEO-ycCxfkDMla1rITC1imJFdoEHiZTovdxhhmaO3_p-DYQOZ2BgvoFuiNW85ctrBwmzJeAlBgkBp8',
+    "lwa_app_id": None,
+    "lwa_client_secret": None,  # Amazon Creds
+    "refresh_token": None,
 }
 
 seller_id = None  # Seller ID
@@ -18,8 +18,8 @@ shop_name = 'Donald'  # Shop name
 
 """ --- Google settings --- """
 google_creds_path = './credentials/google_creds.json'  # Google Creds
-spreadsheet_id = '1aztiN2AU115Ed7o6At3iQIXgVd0Fl0qk5rj1lqpeH1w'  # Spreadsheet ID
-main_worksheet = 'LISTINGS'  # Main worksheet name
+spreadsheet_id = None  # Spreadsheet ID
+main_worksheet = None  # Main worksheet name
 exceptions_worksheet = 'Exceptions'  # Worksheet with exceptions
 exceptions_repricer_worksheet = 'Exceptions for Repricer'  # Worksheet with exceptions for "Repricer.com"
 col_names = {'asin': 'asin',
