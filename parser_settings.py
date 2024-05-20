4fc642ef88dcadbbe30792ae4968cf77eec4580f
ltrix07 <skotarenko138@gmail.com>
2024-05-20 23:55:36 +0300

    global fix

--- parser_settings.py
+++ parser_settings.py
@@ -19,7 +19,7 @@ headers = {
     'sec-fetch-site': 'cross-site',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
 }
 
 
